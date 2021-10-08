9d436d6c2a70b2756ccd78f20142cd38f7a879e1
Rebekah Munoz Warburton <bex.warburton@gmail.com>
2021-10-08 15:10:54 +0200

    version without props

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,4 @@
-/* Header component */
-const Header = {
-    template: `
-         <div class="header">
-             <h1 class="leftContent">Bex's Weather</h1>
-             <p class="rightContent">Five Day Forecast</p>
-         </div>
-         `
-    ,
-}
-
-/* City Name Geo Loc component */
+/* Weather display main component */
 const Weather = {
     // Data
     data() {
@@ -17,6 +6,7 @@ const Weather = {
             // main weather statistics
             cityName: "London",
             iconID: "",
+            dateToday: "",
             minTemp: 0,
             maxTemp: 0,
             windSpeed: 0,
@@ -33,8 +23,9 @@ const Weather = {
         let data = await res.json();
         console.log(data);
 
-        for (i = 0; i < 5; i++) {
+        for (i = 0; i < 26; i++) {
             const newDay = {
+                dateToday: data.list[i].dt_txt,
                 minTemp: data.list[i].main.temp_min,
                 maxTemp: data.list[i].main.temp_max,
                 windSpeed: data.list[i].wind.speed,
@@ -46,34 +37,28 @@ const Weather = {
         };
     },
     // computed values
-    computed: {
-        // today's date
-        todayDate() {
-            // index will be used to related the following day/month words
-            const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-            const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-            // Calculate all necessary measurements
-            const time = new Date();
-            const month = time.getMonth();
-            const date = time.getDate();
-            const day = time.getDay();
-            const hour = time.getHours();
-            const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour
-            const minutes = time.getMinutes();
-            const ampm = hour >= 12 ? 'PM' : 'AM'
-            // create time and date displays
-            const timeNow = (hoursIn12HrFormat < 10 ? '0' + hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + `${ampm}`
-            const dateToday = days[day] + ', ' + date + ' ' + months[month]
-            // create final date stamp
-            this.dateStamp = dateToday + " at " + timeNow
-            return this.todayDate
-        },
-        /* generateIcon(newDay) {
-            const iconID = newDay.iconName
-            this.icon = "http://openweathermap.org/img/wn/${iconId}@2x.png"
-            return this.icon;
-        } */
-    },
+    /* computed: { */
+    // function to display date as 07/10/2021 at 04.20 pm
+    /* todayDate() {
+        // index will be used to related the following day/month words
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        // Calculate all necessary measurements
+        const time = new Date();
+        const month = time.getMonth();
+        const date = time.getDate();
+        const day = time.getDay();
+        const hour = time.getHours();
+        const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour
+        const minutes = time.getMinutes();
+        const ampm = hour >= 12 ? 'PM' : 'AM'
+        // create time and date displays
+        const timeNow = (hoursIn12HrFormat < 10 ? '0' + hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + `${ampm}`
+        const dateToday = days[day] + ', ' + date + ' ' + months[month]
+        // create final date stamp
+        this.dateStamp = dateToday + " at " + timeNow
+        return this.todayDate
+    }, */
     // Methods
     methods: {
         // on click, load weather data for geo localisation
@@ -88,7 +73,7 @@ const Weather = {
                 const data = await res.json();
                 console.log(data)
                 /* display data in cards */
-                for (i = 0; i < 5; i++) {
+                for (i = 0; i < 26; i++) {
                     const newDay = {
                         minTemp: data.list[i].main.temp_min,
                         maxTemp: data.list[i].main.temp_max,
@@ -100,6 +85,7 @@ const Weather = {
                 };
             });
         },
+        // function called when input city name is added
         async typedName() {
             const url = "https://api.openweathermap.org/data/2.5/weather?q="
                 + this.typedCityName
@@ -114,36 +100,28 @@ const Weather = {
                     minTemp: data.list[i].main.temp_min,
                     maxTemp: data.list[i].main.temp_max,
                     windSpeed: data.list[i].wind.speed,
-                    description: data.list[i].weather[0].description
+                    description: data.list[i].weather[i].description
                 };
                 console.log(newDay);
                 this.weatherForecast.push(newDay);
             };
         },
-        // set weather data in card with optional chaining
-        /* setData(data) {
-            this.cityName = data?.name;
-            this.minTemp = data?.main?.temp_min;
-            this.maxTemp = data?.main?.temp_max;
-            this.windSpeed = data?.wind?.speed;
-            this.description = data?.weather[0]?.description;
-            // ADD ICON
-            // ADD FUNCTION for DATE + 1
-        }, */
     },
     template: `
         <div>    
-            <div class="buttons">
+            <div class="header">
+                <h1 class="leftContent">Bex's Weather</h1>
+                <p class="rightContent">Five Day Forecast</p>
                 <button class="findButton" @click="geoLocalisation">Get My Location</button>
                 <label class="dateDisplay labelTyped" for="typedCityName">Type your city:</label>
                 <input v-model="typedCityName" class="typedCityInput" name="typedCityName" @keyup.enter="typedName"/>
             </div>
-            <p class="dateDisplay">{{ todayDate }}</p>    
+             
             <h2>{{ cityName }}</h2>
             <div class="cards">
                 <div class="card" v-for="(weatherDay, index) in weatherForecast" :key="index">
-                    
-                    <img :src="'http://openweathermap.org/img/wn/' + weatherDay.iconID + '@2x.png'">
+                    <p class="dateDisplay">{{ weatherDay.dateToday }}</p>   
+                    <img style="icon" :src="'http://openweathermap.org/img/wn/' + weatherDay.iconID + '@2x.png'">
                     <div class="container">
                         <p class="weatherData">
                             <ion-icon name="thermometer-outline" size="large"></ion-icon> 
@@ -179,7 +157,6 @@ const Footer = {
 const MainComponent = {
     // Components
     components: {
-        "add-header": Header,
         "weather": Weather,
         "add-footer": Footer
     },
